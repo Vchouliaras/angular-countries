e24662423d987036a4556dc0ecbd9bf43eca62d5
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2016-08-08 18:48:50 +0300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCountriesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularCountriesApp
+ */
+angular.module('angularCountriesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCountriesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularCountriesApp
+ */
+angular.module('angularCountriesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
