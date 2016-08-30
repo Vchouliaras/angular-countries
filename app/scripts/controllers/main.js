a50f09eec641afea165fdd7aa33d94ca8b9930f4
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2016-08-30 03:38:14 +0300

    Finishes unit testing for MainCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,6 @@
 angular.module('angularCountriesApp')
     .controller('AppMainCtrl', ['$rootScope','$scope','$filter', '$mdSidenav',
     function ($rootScope, $scope, $filter, $mdSidenav) {
-
     // Register filters.
     var orderByFilter = $filter('orderBy');
     var filterFilter = $filter('filter');
@@ -14,32 +13,32 @@ angular.module('angularCountriesApp')
       $scope.SearchInput.appear = (!$scope.SearchInput.appear) ? true : false;
     };
 
+
     // Search input terms.
     $scope.selectedSearchInputTerm = '';
-    $scope.selectedSearchInputTerm = function(value) {
+    $scope.selectedSearchInputTerm = function(value, rootScope = $rootScope) {
       var expression;
-      $rootScope.Countries = $rootScope.CountriesTemp;
-      $rootScope.Countries = filterFilter($rootScope.Countries,
+      rootScope.Countries = filterFilter(rootScope.Countries,
         function(item) {
           expression = (item.name + ' ' + item.capital).toLowerCase().trim();
           return (expression.search(value.toLowerCase().trim()) !== -1) ? true : false;
         });
     };
 
-    $scope.Populationfilters = ['From High to Low', 'From Low to High'];
     // When a Population filter is selected.
-    $scope.selectedPopulationFilterChanged = function(value) {
+    $scope.Populationfilters = ['From High to Low', 'From Low to High'];
+    $scope.selectedPopulationFilterChanged = function(value, rootScope = $rootScope) {
       $scope.selectedAreaFilter = '';
       value = (value === 'from_low_to_high') ? '+' : '-';
-      $rootScope.Countries = orderByFilter($rootScope.Countries, value + 'population');
+      rootScope.Countries = orderByFilter(rootScope.Countries, value + 'population');
     };
 
     $scope.Areafilters = ['From Big to Small', 'From Small to Big'];
     // When a Area filter is selected.
-    $scope.selectedAreaFilterChanged = function(value) {
+    $scope.selectedAreaFilterChanged = function(value, rootScope = $rootScope) {
       $scope.selectedPopulationFilter = '';
       value = (value === 'from_small_to_big') ? '+' : '-';
-      $rootScope.Countries = orderByFilter($rootScope.Countries, value + 'area');
+      rootScope.Countries = orderByFilter(rootScope.Countries, value + 'area');
     };
 
     $scope.searchCurrencyTerm = {term: ''};
@@ -57,20 +56,21 @@ angular.module('angularCountriesApp')
     };
 
     // When a new Currency filter is selected.
-    $scope.selectedCurrencyFilterChanged = function(value) {
-      $rootScope.Currencyfilters = $rootScope.CurrencyfiltersTemp;
-      $rootScope.Countries = $rootScope.CountriesTemp;
+    $scope.selectedCurrencyFilterChanged = function(value, rootScope = $rootScope) {
+      rootScope.Currencyfilters = rootScope.CurrencyfiltersTemp;
+      // rootScope.Countries = rootScope.CountriesTemp;
       if (value.length > 0) {
         var tempArr = [];
         angular.forEach(value, function(option) {
-          tempArr = tempArr.concat(filterFilter($rootScope.Countries, {currencies: option}));
-          // Remove duplicate values from tempArr to avoid duplicate entries error.
+          tempArr = tempArr.concat(filterFilter(rootScope.Countries, {currencies: option}));
+          // Remove duplicate values from tempArr
+          // to avoid duplicate entries error.
           tempArr.filter(function(el) {
               return tempArr.indexOf(el) === -1;
             });
 
         });
-        $rootScope.Countries = tempArr;
+        rootScope.Countries = tempArr;
       }
     };
 
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -4,25 +4,52 @@
 describe('Controller: MainCtrl', function () {
 
   var MainCtrl, scope;
+  var Countries = [
+    {name : 'Germany',capital: 'Berlin', currencies:['EUR'], population: 20000000, area: 2000},
+    {name: 'Greece', capital: 'Athens', currencies:['USD'], population: 10000000, area: 1000}];
 
   // Load the controller's module.
   beforeEach(module('angularCountriesApp'));
 
   // Load the controller.
-  beforeEach(inject(function ($controller) {
-    scope = {};
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
     MainCtrl = $controller('AppMainCtrl', {
-      $scope: scope
+      $scope: scope,
     });
   }));
 
-  it('The Search input should appear on click', function(){
-    var default_value = scope.SearchInput;
-    scope.toggleSearch();
-    var actual_value = scope.SearchInput;
-    expect(default_value.appear).toBe(actual_value.appear);
+  // Populate scope.
+  beforeEach(function(){
+    scope.Countries = Countries;
   });
 
+  it('The Search input should appear on click', function(){
+    scope.SearchInput.appear = false;
+    scope.toggleSearch();
+    expect(scope.SearchInput.appear).toBe(true);
+  });
 
+  // @TODO need to write a custom matcher for more results.
+  it('Search should contain \"Greece\" when I search the term \"Gre\"', function() {
+    scope.selectedSearchInputTerm('Gre', scope);
+    expect(scope.Countries[0].name).toContain('Greece');
+  });
+
+  it('Population filter should sort from High to Low when the corresponding option is selected', function() {
+    scope.selectedPopulationFilterChanged('from_high_to_low', scope);
+    expect(scope.Countries[0].population).toBe(20000000);
+  });
+
+  it('Area filter should sort from Big to Small when the corresponding option is selected', function() {
+    scope.selectedAreaFilterChanged('from_big_to_small', scope);
+    expect(scope.Countries[0].area).toBe(2000);
+  });
+
+  it('When EUR currency is selected, only countries with corresponding currency should be displayed', function() {
+    scope.selectedCurrencyFilterChanged(["EUR"], scope);
+    console.log(scope.Countries);
+    expect(scope.Countries[0].currencies[0]).toMatch('EUR');
+  });
 
 });
